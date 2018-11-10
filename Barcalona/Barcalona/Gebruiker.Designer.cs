299294dc322ca96f3b25b2ce889c556deaf60d74
Kadir yildrim <kadir.yildrim@hotmail.com>
2018-11-10 19:33:40 +0100

    De form mooier gemaakt

--- Barcalona/Barcalona/Gebruiker.Designer.cs
+++ Barcalona/Barcalona/Gebruiker.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Gebruiker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(694, 450);
+            this.Name = "Gebruiker";
+            this.Text = "Barcalona";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
